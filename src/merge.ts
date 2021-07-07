1bb4a5a90a882294bf26633c83b52e7290c14859
arnoson <hallo@arno-schlipf.info>
2021-07-07 18:02:11 +0200

    Use fragment in head

--- src/merge.ts
+++ src/merge.ts
@@ -11,9 +11,11 @@ export const merge = (target: HTMLElement, source: HTMLElement) => {
   const targetNodes = Array.from(target.children)
   const sourceNodes = Array.from(source.children)
 
+  const fragment = new DocumentFragment()
   for (const node of sourceNodes) {
     if (node.tagName !== 'TITLE' && !containsNode(targetNodes, node)) {
-      target.append(copyNode(node))
+      fragment.append(copyNode(node))
     }
   }
+  target.append(fragment)
 }
