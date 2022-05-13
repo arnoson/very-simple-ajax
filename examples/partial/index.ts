51c4b1520fe6522e6afc2fa3b4bc741cc819028c
arnoson <hallo@arno-schlipf.info>
2022-05-13 18:39:27 +0200

    Rename `dev` to `examples`

--- examples/partial/index.ts
+++ examples/partial/index.ts
@@ -1,12 +1,14 @@
-import links from '../../src'
+import * as links from '../../src'
 
-const initPage = () =>
+const initPage = () => {
+  console.log('init page')
   document.querySelectorAll('a').forEach((el) =>
     el.addEventListener('click', async (event) => {
       event.preventDefault()
       links.visit(el.getAttribute('href'))
     })
   )
+}
 
 document.addEventListener('very-simple-links:visit', initPage)
 document.addEventListener('DOMContentLoaded', initPage)
