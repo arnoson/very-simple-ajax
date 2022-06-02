59a2b4834184915716c725b1770a7a593c78eee3
arnoson <hallo@arno-schlipf.info>
2022-06-02 15:39:04 +0200

    Don't duplicate `<meta>`
    There should exist only one `<meta>` element with a certain name.

--- src/utils/merge.ts
+++ src/utils/merge.ts
@@ -1,22 +1,38 @@
 import { copyScript } from './copyScript'
+import { replaceWith } from './replaceWith'
 
-const containsNode = (nodes: Element[], node: Element) =>
+const containsEqualNode = (nodes: Element[], node: Element) =>
   nodes.find((el) => el.isEqualNode(node))
 
 /**
- * Add all new children from source to target. Ignore `<title>` as there should
- * only be one `<title>` per document.
+ * Add all new children from source to target.
  */
 export const merge = (target: HTMLElement, source: HTMLElement) => {
   const targetNodes = Array.from(target.children)
   const sourceNodes = Array.from(source.children)
 
+  const metaNodesByName = new Map(
+    Array.from(target.getElementsByTagName('meta')).map((el) => [el.name, el])
+  )
+
   const fragment = new DocumentFragment()
   for (const node of sourceNodes) {
-    if (node.tagName !== 'TITLE' && !containsNode(targetNodes, node)) {
-      // Force the browser to execute any new scripts.
+    if (node.tagName !== 'TITLE' && !containsEqualNode(targetNodes, node)) {
+      const isMeta = node instanceof HTMLMetaElement
       const isScript = node instanceof HTMLScriptElement
-      fragment.append(isScript ? copyScript(node) : node)
+
+      if (isMeta) {
+        const targetNode = metaNodesByName.get(node.name)
+        if (targetNode) {
+          // There should exist only one `<meta>` element with a certain name.
+          replaceWith(targetNode, node)
+        } else {
+          fragment.append(node)
+        }
+      } else {
+        // Force the browser to execute any new scripts.
+        fragment.append(isScript ? copyScript(node) : node)
+      }
     }
   }
   target.append(fragment)
