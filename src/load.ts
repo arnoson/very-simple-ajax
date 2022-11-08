8c0932b1592d845e2c2db377e08bf0f8348332c3
arnoson <omasocke@hotmail.de>
2022-11-08 15:04:48 +0100

    feat: show actual progress instead of trickle

--- src/load.ts
+++ src/load.ts
@@ -1,20 +1,45 @@
 const parser = new DOMParser()
 let progress = 0
-let trickleInterval: number | undefined
 
 export const load = async (url: string): Promise<Document | undefined> => {
   try {
     setProgress(0)
-    const progressDelayTimeout = window.setTimeout(() => {
-      toggleLoading(true)
-      startTrickle()
-    }, 500)
+    // Only show the progress bar if the page takes more than 500ms to load.
+    const progressDelayTimeout = window.setTimeout(
+      () => toggleLoading(true),
+      500
+    )
 
-    const response = await fetch(url)
-    const html = parser.parseFromString(await response.text(), 'text/html')
+    const response = await fetch(url, { headers: { 'X-Very-Simple': '1' } })
+    const reader = response.body?.getReader()
+    const length = parseInt(response.headers.get('Content-Length') ?? '0')
+    let receivedBytes = 0
+
+    const stream = new ReadableStream({
+      start(controller) {
+        const read = async () => {
+          const { done, value } = await reader!.read()
+
+          if (done) {
+            controller.close()
+            return
+          }
+
+          receivedBytes += value.length
+          setProgress(receivedBytes / length)
+          controller.enqueue(value)
+
+          read()
+        }
+        read()
+      },
+    })
+
+    const html = await new Response(stream, {
+      headers: { 'Content-Type': 'text/html' },
+    }).text()
 
     clearTimeout(progressDelayTimeout)
-    stopTrickle()
     setTimeout(() => {
       // Without a small timeout transitions won't work on a permanent progress
       // bar.
@@ -22,22 +47,13 @@ export const load = async (url: string): Promise<Document | undefined> => {
       toggleLoading(false)
     })
 
-    return html
+    return parser.parseFromString(html, 'text/html')
   } catch (e) {
     // There was a network error. We reload the page so the user sees the
     // browser's network error page.
     window.location.reload()
   }
 }
-
-const trickle = () => {
-  const amount = -0.095 * progress + 0.1
-  setProgress(progress + Math.random() * amount)
-}
-
-const startTrickle = () => (trickleInterval = window.setInterval(trickle, 300))
-
-const stopTrickle = () => clearInterval(trickleInterval)
 
 const setProgress = (value: number) => {
   progress = value
