3c42c93532be595cb4e526683611fe094aba7139
arnoson <omasocke@hotmail.de>
2022-11-14 12:29:32 +0100

    chore: test on chrome only
    testing multiple browser lead to false negative tests, maybe parallel
    usage of links doesn't work?

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,14 +3,9 @@ import { devices } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   testDir: './test',
-
-  use: { baseURL: 'http://127.0.0.1:5173' },
+  webServer: { command: 'vite', port: 5173 },
+  use: { ...devices['Desktop Chrome'] },
   timeout: 5 * 1000,
-
-  projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-  ],
 }
 
 export default config
