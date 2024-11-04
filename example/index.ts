e62962ef2fff7750ae01a69ce8d60d7c5e07de75
arnoson <hallo@arno-schlipf.info>
2024-11-04 13:37:26 +0100

    chore: prevent re-initializing links in example

--- example/index.ts
+++ example/index.ts
@@ -1,12 +1,16 @@
 import links from '../src'
 
 const initPage = () =>
-  document.querySelectorAll('a').forEach((el) =>
+  document.querySelectorAll('a').forEach((el) => {
+    // @ts-ignore
+    if (el.$init) return
     el.addEventListener('click', async (event) => {
       event.preventDefault()
       links.visit(el.getAttribute('href')!)
     })
-  )
+    // @ts-ignore
+    el.$init = true
+  })
 
 document.addEventListener('DOMContentLoaded', initPage)
 links.on('visit', initPage)
