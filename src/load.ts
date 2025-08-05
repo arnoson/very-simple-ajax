21d95d49514f03f7d75c9cdef8d78b5bc5d6cc70
arnoson <hallo@arno-schlipf.info>
2025-08-05 11:24:24 +0200

    fix: don't reload page if request is canceled

--- src/load.ts
+++ src/load.ts
@@ -11,7 +11,7 @@ export const load = async (
     // We only allow one pending request at a time. When triggering a new request
     // while the last one is still pending we mimic browser behavior and cancel
     // the pending one.
-    currentLoadController?.abort()
+    currentLoadController?.abort('cancel')
     currentLoadController = new AbortController()
 
     setProgress(0)
@@ -66,8 +66,7 @@ export const load = async (
     return parser.parseFromString(html, 'text/html')
   } catch (e) {
     setProgress(0)
-    const isAbortError = e instanceof DOMException && e.name === 'AbortError'
-    if (isAbortError || currentLoadController?.signal.aborted) {
+    if (e !== 'cancel') {
       // There was a network error. We reload the page so the user sees the
       // browser's network error page.
       window.location.reload()
